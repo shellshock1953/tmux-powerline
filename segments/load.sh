e87cae14b015fe597a048e3ba10d14b116228dd3
bsukho <bsukho@softserveinc.com>
2019-07-31 17:05:29 +0300

    fix load1

--- segments/load.sh
+++ segments/load.sh
@@ -1,6 +1,7 @@
 # Print the average load.
 
 run_segment() {
-	uptime | cut -d "," -f 4 | cut -d ":" -f2- | sed -e "s/^[ \t]*//"
+	# uptime | cut -d "," -f 4 | cut -d ":" -f2- | sed -e "s/^[ \t]*//"
+    uptime | awk '{print $10}' | awk -F \, '{print $1"."$2}'
 	exit 0
 }
